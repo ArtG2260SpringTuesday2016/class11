c91eb65804020b3559519e6168718db149ba0c30
heyryanleys <ryanleys@gmail.com>
2016-03-29 20:20:54 -0400

    Simplified Code

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
-var data = myData
+var data = [5,25,50];
+var yPosn = 700;
+var rectWidth = 45;
 
 function draw(){
   createCanvas(600,700);
     for (var i=0; i < data.length; i++){
       var xPosn = 70*(i);
-      var yPosn = 700;
-      var rectWidth = 45;
       var rectHeight= -data[i]*13;
-      var colors = collidePointRect(mouseX,mouseY,xPosn,yPosn + rectHeight,rectWidth, rectHeight);
+      var colors = collidePointRect(mouseX,mouseY,xPosn,yPosn + rectHeight,rectWidth, -rectHeight);
         if(colors){
             fill(0)
           }else{
