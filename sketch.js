5345d376242e1008b5042654a61ef42a6b540bd8
heyryanleys <ryanleys@gmail.com>
2016-03-29 20:13:52 -0400

    Three Shapes
    Hope this is right

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
-function setup() {
-  
-}
+var data = myData
 
-function draw() {
-  
+function draw(){
+  createCanvas(600,700);
+    for (var i=0; i < data.length; i++){
+      var xPosn = 70*(i);
+      var yPosn = 700;
+      var rectWidth = 45;
+      var rectHeight= -data[i]*13;
+      var colors = collidePointRect(mouseX,mouseY,xPosn,yPosn + rectHeight,rectWidth, rectHeight);
+        if(colors){
+            fill(0)
+          }else{
+            fill(120,50,20);
+          }
+  noStroke();
+  rect(xPosn, yPosn, rectWidth, rectHeight);
+  }
 }
