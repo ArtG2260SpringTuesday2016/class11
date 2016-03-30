eaa7d7c226069dde799824c6691a577ea51102bb
MistaFish31 <zhang.ma@husky.neu.edu>
2016-03-29 20:26:16 -0400

    something..

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+//1. Put 3 shapes on the screen; when you mouse over them they change color
+//2. Refactor so you pass the same objects into both the function that renders and detects hits
+
+var myRect =function(){
+    this.x=20;
+    this.y=20;
+    this.width=50;
+    this.height=50;
+    this.color="aliceBlue"
+    
+    this.renderRect=function(){
+      fill(color);
+      rect(this.x,this.y,this.width,this.height);
+    };
+
 function setup() {
-  
+  createCanvas(700, 700);
+  }
 }
 
 function draw() {
-  
+  ellipse
 }
