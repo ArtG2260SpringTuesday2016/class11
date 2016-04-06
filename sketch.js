5fb188596eedf5f19ab4bd9105775009965f0611
MistaFish31 <zhang.ma@husky.neu.edu>
2016-04-05 20:01:32 -0400

    color change collision

--- sketch.js
+++ sketch.js
@@ -1,29 +1,43 @@
 //1. Put 3 shapes on the screen; when you mouse over them they change color
 //2. Refactor so you pass the same objects into both the function that renders and detects hits
 
-var myRect =function(){
-    this.x=20;
-    this.y=20;
-    this.width=50;
-    this.height=50;
-    this.color="aliceBlue"
-    
-    this.renderRect=function(){
-      fill(color);
-      rect(this.x,this.y,this.width,this.height);
-    };
-    
-    this.isMouseOnRect=function(){
-      if(collidePointRect(mouseX,mouseY,this.x,this.y,this.width,this.height)){
-        this.color="#333"
-      }
-    }
+var hit=false;
+var info=[10,20,60,5,10,8,13,10,19,13];
+
 
 function setup() {
-  createCanvas(700, 700);
-  }
+  createCanvas(900, 900);
 }
 
 function draw() {
-  ellipse
+  // var posnX=20;
+  // var posnY=250;
+  // var rectHeight=30;
+  // var rectWidth=40;
+  
+  var spacing=10
+  for(var i=0; i<info.length; i++){
+    spacing+=50
+    var dramaticHeight=info[i]*10
+    strokeWeight(2);
+    stroke(10,10,10);
+    
+    hit=collidePointRect(mouseX,mouseY,spacing,100,30,dramaticHeight)
+    
+    if(hit){
+      fill(250,10,10);
+    }else{
+      fill(10,10,10);
+    }
+    rect(spacing,100,30,dramaticHeight);
+  }
+  
+  
+  
+//  var collision {
+//  if (collideCircleCircle(mouseX,mouseY,20,304,220,20){
+//  }
+  
+  
+  
 }
